bb0b964b11136d9282c987b0e13bba5c4715b8a0
slikhub <clonage@gmail.com>
2022-07-26 05:57:23 -0400

    ho

--- games.js
+++ games.js
@@ -11,3 +11,118 @@ var rplanet_name = "Rplanet";
 var rplanet_img = "QmegodhiwaGMsKhCe74WjFtojrm7fPoFHP4w7HBJLd5qAP";
 var rplanet_gameurl = "https://rplanet.io/lab";
 var rplanet_gamewallet = "rplanet";
+
+var crypto4shots_name = "Crypto4Shots";
+var crypto4shots_img = "QmTGn2Y4kgmwKh2zhJdndoe63CS55wXtNaZguzVzS7rBzY";
+var crypto4shots_gameurl = "https://play.crypto-shots.com";
+var crypto4shots_gamewallet = "crypto4shots";
+
+var xpansiongame_name = "Xpansion";
+var xpansiongame_img = "QmXbvH4QWrg9gAiWWT8V5ymFokuLs7tjLHphc2BdVHdgV3";
+var xpansiongame_gameurl = "https://play.xpsgame.io/";
+var xpansiongame_gamewallet = "xpansiongame";
+
+var onmars_name = "OnMars";
+var onmars_img = "QmYsMQkJs9jUzQUexJSnyQd5bfdvKP3Fx5VWHrRskW7pjp";
+var onmars_gameurl = "https://milliononmars.io";
+var onmars_gamewallet = "onmars";
+
+var miningntwrkc_name = "MiningNetwork";
+var miningntwrkc_img = "Qmc3pFSUmBFsDWHepr95jPCNjCYgjVZoJVf3pXLSBrdbQq";
+var miningntwrkc_gameurl = "https://miningnetwork.io/";
+var miningntwrkc_gamewallet = "miningntwrkc";
+
+var farmersworld_name = "FarmersWorld";
+var farmersworld_img = "QmX79zrJsk4DbWQ3krgu41pX3fdvEvWjkMXiNCKpxFXSgj";
+var farmersworld_gameurl = "https://play.farmersworld.io/";
+var farmersworld_gamewallet = "farmersworld";
+
+var freecitygame_name = "FreeCityGame";
+var freecitygame_img = "QmVRyVWKjwm5fT7EZ8J5kL4LY5YLLpeuxxBedivxqBH3s3";
+var freecitygame_gameurl = "https://play.remixworlds.com/";
+var freecitygame_gamewallet = "freecitygame";
+
+var immersys_name = "ImmerSys";
+var immersys_img = "QmNxiNpqcLjePmVS8tURdtD95ysVsujWwWFiiZzckmjagc";
+var immersys_gameurl = "https://companion.immersys.io";
+var immersys_gamewallet = "immersys";
+
+var arenaofglory_name = "ArenaOfGlory";
+var arenaofglory_img = "QmV6sqDaPmYyfHtHEXr4L7kBoXAS1cSf8cg9UJCekDpZSk";
+var arenaofglory_gameurl = "https://play.arenaofglory.io";
+var arenaofglory_gamewallet = "arenaofglory";
+
+var diggersworld_name = "DiggersWorld";
+var diggersworld_img = "QmbCuQUExo844XKpTcPotncp8fCa1rx168uWXMVFyoKhVH";
+var diggersworld_gameurl = "https://play.diggersworld.io";
+var diggersworld_gamewallet = "diggersworld";
+
+var greenrabbit_name = "GreenRabbit";
+var greenrabbit_img = "QmZjuF9Y8p4287T1nB8RQCZ6kWLFX3CW5jMADAtWG3KWTX";
+var greenrabbit_gameurl = "https://play.greenrabbitgame.io";
+var greenrabbit_gamewallet = "greenrabbit";
+
+var gnocityworld_name = "GnoCity";
+var gnocityworld_img = "QmNc4yhWiqkeTs8DZzgSKcarRYkS8cA6zB4yZQV9d3uV4a";
+var gnocityworld_gameurl = "https://www.gnocity.io/";
+var gnocityworld_gamewallet = "gnocityworld";
+
+var gnocityworld_name = "AgeOfFarming";
+var gnocityworld_img = "Qma4EcBu4GtVuRZL6qR8m8xnNqSVaGi8SCqctfynwyVKet";
+var gnocityworld_gameurl = "https://play.ageoffarming.io";
+var gnocityworld_gamewallet = "ageoffarming";
+
+var funnycatgame_name = "FunnyCat";
+var funnycatgame_img = "QmeiCMAs1qXcH4TGBmGkMiuxN5pkLNuu67NbwMe84HM92V";
+var funnycatgame_gameurl = "https://play.funnycat.io";
+var funnycatgame_gamewallet = "funnycatgame";
+
+var nftpandawaxp_name = "NFTPanda";
+var nftpandawaxp_img = "QmR1haurnG96XXg738g5nbYFpB7hQ11Afe7b75anRPjQLh";
+var nftpandawaxp_gameurl = "https://game.nftpanda.space";
+var nftpandawaxp_gamewallet = "nftpandawaxp";
+
+var goldmandgame_name = "GoldMand";
+var goldmandgame_img = "QmVd9iUzNF4x5LUNby6uLu4BU2NFCXLCYMotH9mc1g7N7a";
+var goldmandgame_gameurl = "https://play.goldmand.io";
+var goldmandgame_gamewallet = "goldmandgame";
+
+var realmnftgame_name = "RealmNFT";
+var realmnftgame_img = "QmPczbhZ9SSqfBap2DAffr3bf4dDkwh8fABWoYpYz9kvHJ";
+var realmnftgame_gameurl = "https://play.realmnft.io";
+var realmnftgame_gamewallet = "realmnftgame";
+
+var dragonvalley_name = "DragonsValley";
+var dragonvalley_img = "QmTBM9Ep5yMwW4cDZKgap5JN7vTnSQoM1H9YpWdGFEn6Ry";
+var dragonvalley_gameurl = "https://play.dragonsvalley.io";
+var dragonvalley_gamewallet = "dragonvalley";
+
+var tokenlands11_name = "TokenLands";
+var tokenlands11_img = "QmP8MdoZ8VfG7cwN821LkPRxKoWW1b2v76mnCUgDJmfKDC";
+var tokenlands11_gameurl = "https://playing.tokenlands.io/en/play/";
+var tokenlands11_gamewallet = "tokenlands11";
+
+var bcbrawlers_name = "BCBrawlers";
+var bcbrawlers_img = "QmRkWofHFCkEZaeZ9MCWYA5GxPfaDJouKfN3NHdLUVLsuw";
+var bcbrawlers_gameurl = "https://play.bcbrawlers.com/";
+var bcbrawlers_gamewallet = "bcbrawlers";
+
+var desertfarmio_name = "DesertFarm";
+var desertfarmio_img = "QmeRLXrEUv71ftp2tYHnfS1zpvdR6NffqZGbkAPPsA74sJ";
+var desertfarmio_gameurl = "https://play.desertfarm.io/?ref=waxdashwaxio";
+var desertfarmio_gamewallet = "desertfarmio";
+
+var clashdomenft_name = "ClashDome";
+var clashdomenft_img = "QmPjmrxQcCv9Nu9KozhCQCntTon1p7FYLWaaFBJRw85JDs";
+var clashdomenft_gameurl = "https://clashdome.io/";
+var clashdomenft_gamewallet = "clashdomenft";
+
+var centurytrain_name = "CenturyTrain";
+var centurytrain_img = "QmamNjsRFtrY4sbL3QBS512XQ99XLHsTqqeGVa8sQP2pTq";
+var centurytrain_gameurl = "https://play.trains.cards";
+var centurytrain_gamewallet = "centurytrain";
+
+var brpg_name = "BlockchainRPG";
+var brpg_img = "QmXzqjsHoCNT7K5UR7JDVxGcCVsM3WwN8DsDWfPSTKXtti";
+var brpg_gameurl = "https://play.blockchainrpg.io";
+var brpg_gamewallet = "brpg";
